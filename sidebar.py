737aae0487ef013707622e79ae5392ce04b82afe
prathmzz <prathameshcode58@gmail.com>
2024-03-30 12:33:48 +0530

    enhanced side bar
    added icons  and font of buttons in side bar

--- sidebar.py
+++ sidebar.py
@@ -1,21 +1,24 @@
 from tkinter import *
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 import subprocess
+
 def open_lecture_page():
     print("Opening Lecture Page...")
     subprocess.run(["python", "lecture_page.py"])
+
 def open_Volunteer_page():
     print("Opening Volunteer Page...")
     subprocess.run(["python", "Volunteer_page.py"])
+
 def open_teacher_section():
     print("Opening Teacher Section Page...")
     subprocess.run(["python", "teacher_section.py"])
+
 def open_Rescue():
     print("Opening Rescue Page...")
     subprocess.run(["python", "Rescue.py"])
 
-
-def create_sidebar(root,open_Volunteer_page,open_lecture_page,open_Rescue,open_teacher_section):
+def create_sidebar(root, open_Volunteer_page, open_lecture_page, open_Rescue, open_teacher_section):
     # Create the top bar (navbar)
     topbar = Frame(root, bg="#ed5876", width=600, height=50)
     topbar.pack(side=TOP, fill=X)
@@ -37,18 +40,19 @@ def create_sidebar(root,open_Volunteer_page,open_lecture_page,open_Rescue,open_t
     sidebar.pack(side=LEFT, fill=Y)
 
     menu_items = [
-        ("Volunteer", "home_icon.png", open_Volunteer_page),
-        ("Donation", "lecture_icon.png", open_lecture_page),
-        ("Rescue", "user_icon.png", open_teacher_section),
-        ("Adoption", "teacher_icon.png", open_Rescue),
+        ("Volunteer", "volunteer.png", open_Volunteer_page),
+        ("Donation", "donation.png", open_lecture_page),
+        ("Rescue", "animal-rescue.png", open_teacher_section),
+        ("Adoption", "adoption.png", open_Rescue),
     ]
 
     buttons = []
 
     for item in menu_items:
-        image = Image.open("Images/" + item[1])
+        # Resize the image to 50x50 using LANCZOS resampling filter
+        image = Image.open("Images/" + item[1]).resize((50, 50), Image.LANCZOS)
         photo_image = ImageTk.PhotoImage(image)
-        button = Button(sidebar, text=item[0], image=photo_image, compound=LEFT, fg="white", bg="#eb4163", bd=0, padx=20,
+        button = Button(sidebar, text=item[0], font=("Arial", 12, "bold"), image=photo_image, compound=LEFT, fg="white", bg="#eb4163", bd=0, padx=20,
                         pady=10, anchor="w")
         button.image = photo_image
         button.pack(anchor="w", fill=X)
